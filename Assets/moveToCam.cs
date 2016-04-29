18480513f833bed23731a0457623d092bc7de7de
KristofferHolm <klh12@student.aau.dk>
2016-04-29 17:04:49 +0200

    Gun prop work
    it werks

---
+++ Assets/moveToCam.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveToCam : MonoBehaviour {
+
+    public Quaternion adjustVec;
+	// Use this for initialization
+	void Start () {
+       // transform.position = transform.parent.GetChild(0).position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = transform.parent.GetChild(0).position;
+        transform.rotation = SixenseInput.Controllers[0].Rotation* adjustVec;
+    }
+}
